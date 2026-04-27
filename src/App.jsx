b71db51c24dd671e439486d5b7974b5641143995
GabrielStoychev <gabrielstoichev96@gmail.com>
2026-04-27 11:59:48 +0300

    added new paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <p>by Gabriel Stoychev</p>
       </header>
       <MainContent />
     </>
